9cc7f0736c0c091c54e37e9dd09586ba910fca60
lh207155 <492945960@qq.com>
2020-04-28 22:19:46 +0800

    画出了游戏机屏幕的基本样子

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el: '#app',
   data:{
+    bgDragon:'',
     //初始化地图 new Array(20).fill(new Array(10).fill(0)):引用出错
     baseMap : [
       [0,0,0,0,0,0,0,0,0,0],
@@ -68,7 +69,7 @@ var app = new Vue({
     refreshing:false,
     //游戏中标记
     playing:false,
-
+    halt:false
   },
   methods: {
     //方块变形方法
@@ -657,7 +658,7 @@ var app = new Vue({
     //按键按下开启移动
     startMove(e) {
       //避免报错，先判断需要的数据是否存在,是否在暂停
-      if (this.initPos && !this.pause && !this.freshing) {
+      if (this.initPos && !this.pause && !this.refreshing&&!this.halt) {
         //这里只需要结束下落定时器，并设置新的下落定时器
         if (e.key === 'ArrowDown') {
           //主动下落，先关掉被动下落定时器，再自行调用并设置参数
@@ -717,11 +718,15 @@ var app = new Vue({
     //为按键抬起事件分配任务
     keyEvent(e) {
       //重玩键，关闭下落定时器，执行刷新,刷新过程游戏处于refreshing状态
-      if(e.key === 'r'){
-          this.refresh()
+      if(e.key === 'r'&&!this.halt&& !this.refreshing){
+        this.historyScore.push(this.score)
+        this.clearCount = 0
+        this.squareSum = 1
+        this.score = 0
+        this.refresh()
       }
-      //游戏未开始，未暂停，未刷新，并且按下的是空格键
-      if(e.key=== ' '&& !this.refreshing &&!this.pause&&!this.playing){
+      //游戏未开始，未暂停，未刷新,未停顿，并且按下的是空格键
+      if(e.key=== ' '&& !this.refreshing &&!this.pause&&!this.playing&&!this.halt){
         //重置部分参数
         this.playing = true
         clearInterval(this.nativeFallingID)
@@ -734,7 +739,7 @@ var app = new Vue({
       }
       //如果抬起向上键，分配各自方块的变形任务
       // console.log(e.key)
-      if (this.initPos && !this.pause){
+      if (this.initPos && !this.pause&&!this.halt){
         if (e.key === 'ArrowUp') {
           //判断正在下落的方块类型，调用相应的方块变形任务
           switch (this.currentSquare) {
@@ -798,7 +803,7 @@ var app = new Vue({
       this.render()
       this.nativeFallingID = setInterval(() => {
         //不断的判断游戏有没有暂停，如果被暂停了就直接清除定时器并return了
-        if(this.pause){
+        if(this.pause||this.halt){
           clearInterval(this.nativeFallingID)
           return
         }
@@ -831,10 +836,13 @@ var app = new Vue({
             this.playing = false
             this.refresh()
             this.historyScore.push(this.score)
+            this.clearCount = 0
+            this.squareSum = 1
             this.score = 0
           }else{
             //游戏没有结束继续加squareSum,squareSum变动就自动调用square方法继续掉落方块
-            this.squareSum++
+            //如果是消除行短暂停顿，则不累加方块，防止在停顿是掉落方块发生不期望的错误，把方块累加放到isClear里
+            this.halt? null:this.squareSum++
           }
         } else {
           //无触底直接渲染
@@ -846,18 +854,46 @@ var app = new Vue({
     //判断行数组是否存在都为1的情况，即满足消除该行的条件
     isClear() {
       //col.every(block => block === 1)该行都为1，返回真，findIndex找到该行索引
-      let tempCol = this.baseMap.findIndex(col => col.every(block => block === 1))
-      //存在满足条件的数组
-      if (tempCol != -1) {
-        //消除行累计
-        this.clearCount++
-        //从地图数组中删除该行数组
-        this.baseMap.splice(tempCol, 1)
-        //在地图数组中头部添加一个新的数组
-        this.baseMap.unshift(new Array(10).fill(0))
-        //递归继续判断是否任然存在
-        this.isClear()
+      let tempRow=[]
+      this.baseMap.forEach((row,i)=>{
+        if(row.every((block)=>block===1)){
+           tempRow.push(i)
+        }
+      })
+      console.log(tempRow)
+      if(tempRow.length){
+        this.clearCount+=tempRow.length
+        //暂时冻结游戏
+        this.halt = true
+        setTimeout(()=>{
+          tempRow.forEach((i)=>{
+            this.baseMap.splice(i, 1)
+            this.baseMap.unshift(new Array(10).fill(0))
+          })
+          //延迟1秒消除行特效，并开启游戏
+          this.halt = false
+          //把方块累加放到了这里
+          this.squareSum++
+        },1000)
       }
+
+      //存在满足条件的数组
+      // if (tempCol != -1) {
+      //   //消除行累计
+      //   this.clearCount++
+      //   this.halt = true
+      //   //从地图数组中删除该行数组
+      //   // clearInterval(this.nativeFallingID)
+      //   setTimeout(()=>{
+      //     this.baseMap.splice(tempCol, 1)
+      //     //在地图数组中头部添加一个新的数组
+      //     this.baseMap.unshift(new Array(10).fill(0))
+      //     this.isClear()
+      //     this.halt = false
+      //     this.squareSum++
+      //   },500)
+      //   //递归继续判断是否任然存在
+      // }
     },
     //判断游戏是否结束
     isGameOver() {
@@ -876,7 +912,7 @@ var app = new Vue({
       //1-7随机一个数字给当前方块
       this.currentSquare = 'square' + Math.floor(Math.random() * 7 + 1)
       console.log(this.currentSquare)
-      // this.currentSquare = 'square7'
+      // this.currentSquare = 'square1'
       //初始化即将下落的方块
       this.initPos = this[this.currentSquare].map((block) => ({x: block.x, y: block.y}))
 
@@ -896,9 +932,14 @@ var app = new Vue({
               this.$set(this.baseMap[i], j, 1)
             }
             //如果渲染到最上面一行了resolve
-            i===0?clearInterval(timeout1) :--i
-            i===0?resolve(i):null
-          },20)
+            if(i===0){
+              //先resolve再关闭定时器,否则下面的then就抢先执行了
+              resolve(i)
+              clearInterval(timeout1)
+            }else{
+              i--
+            }
+          },50)
         }).then((i)=> {
           //开始从上往下清除刷新
           return new Promise((resolve)=>{
@@ -906,25 +947,57 @@ var app = new Vue({
               for(let j =9;j>=0;j-- ){
               this.$set(this.baseMap[i], j, 0)
               }
-
-              i===19?clearInterval(timeout2):i++
-              if(i===19)resolve(i)
-          },20)
+              if(i===19){
+                //先resolve再关闭定时器,否则下面的then就抢先执行了
+                resolve(i)
+                clearInterval(timeout2)
+              }else{
+                i++
+              }
+          },50)
           })
         }).then((i)=>{
           console.log(i)
           this.refreshing = false
           this.playing = false
           this.pause = false
+          this.dragonAnimation()
         })
-    }
+    },
+    //小恐龙动画
+    dragonAnimation(){
+      let timeoutID
+      //如果游戏没有进行
+      if(!this.playing){
+        //动画执行脚本
+        let script = [1,1,1,1,1,1,1,1,2,2,2,2,2,2,1,1,1,1,1,1,1,1,2,2,2,2,2,2,1,1,1,1,1,1,1,1,2,2,2,2,2,2,1,1,1,1,1,1,1,1,
+          3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,
+        5,5,5,5,5,5,5,5,6,6,6,6,6,6,5,5,5,5,5,5,5,5,6,6,6,6,6,6,5,5,5,5,5,5,5,5,6,6,6,6,6,6,5,5,5,5,5,5,5,5,
+          7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,7,8,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4]
+        let i =0
+        timeoutID = setInterval(()=>{
+          this.bgDragon = `bg-dragon${script[i]}`
+          i++
+          if(i===script.length){
+            i=0
+          }
+          if(this.playing){
+            this.bgDragon = ''
+            clearInterval(timeoutID)
+          }
+        },100)
+      }else{
+        clearInterval(timeoutID)
+      }
+    },
   },
   computed:{
     maxScore(){
       return Math.max(0,...this.historyScore)
-    }
+    },
   },
   watch:{
+    // playing:'dragonAnimation',
     //监听游戏是否暂停
     pause:function () {
       if(!this.pause){
@@ -933,12 +1006,17 @@ var app = new Vue({
       }
     },
     //监听squareSum变化，变化即方块已落下，squareSum+1，则调用square方法继续下落
-    squareSum:'square',
+    squareSum:function () {
+      if(this.squareSum!=1){
+        this.square()
+      }
+    },
   },
   created(){
     //监听键盘事件
     document.onkeydown = this.startMove
     document.onkeyup = this.keyEvent
+    this.dragonAnimation()
 
   }
 })
